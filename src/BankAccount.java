5a695e2d947f0094b84a7ffc0ca37817e681d947
Will Drescher <will_drescher@mines.edu>
2026-03-01 20:16:00 -0700

    BankAccount.java complete

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,8 +1,29 @@
-
 public class BankAccount {
 
-	public BankAccount() {
-		// TODO Auto-generated constructor stub
-	}
+    private double balance;
 
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount) throws NegativeBalanceException {
+        if (amount > balance) {
+            double newBalance = balance - amount; // this will be negative
+            throw new NegativeBalanceException(newBalance);
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public void quickWithdraw(double amount) throws NegativeBalanceException {
+        if (amount > balance) {
+            throw new NegativeBalanceException();
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
