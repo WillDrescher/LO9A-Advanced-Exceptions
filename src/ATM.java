74aca9b9449f2b95535312f1deb1b10fdbb08a56
Will Drescher <will_drescher@mines.edu>
2026-03-01 20:16:35 -0700

    ATM started

--- src/ATM.java
+++ src/ATM.java
@@ -1,8 +1,29 @@
-
 public class ATM {
 
-	public ATM() {
-		// TODO Auto-generated constructor stub
-	}
+    private BankAccount account;
 
+    public ATM() {
+        account = new BankAccount(500.0);
+    }
+
+    public void handleTransactions() {
+        try {
+            account.withdraw(600.0);
+        } catch (NegativeBalanceException e) {
+            System.out.println(e);
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            account.quickWithdraw(600.0);
+        } catch (NegativeBalanceException e) {
+            System.out.println(e);
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.handleTransactions();
+    }
 }
